0ef836eb2631070c6ac1bb5a050e3400db28c241
Surya Teja Tangella <suryatejatangella234@gmail.com>
2026-05-07 06:33:20 +0530

    feat: add BackButton component and integrate it into City and Form components

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,9 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 import { useCities } from "../contexts/CitiesContext";
 import { useEffect } from "react";
 import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -70,7 +71,9 @@ function City() {
         </a>
       </div>
 
-      <div></div>
+      <div>
+        <BackButton/>
+      </div>
     </div>
   );
 }
