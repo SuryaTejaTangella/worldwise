95d42a61e1ab0b209aec77795d521a693665576d
Surya Teja Tangella <suryatejatangella234@gmail.com>
2026-05-15 06:33:56 +0530

    feat: integrate authentication context and update login flow with navigation

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -19,7 +19,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "Surya",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
@@ -51,6 +51,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
